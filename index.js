4abe9491d5e90d0a7565dffeb8bc06f2e7f10ce7
Waahx <148732245+Waahx@users.noreply.github.com>
2024-02-29 23:32:58 -0300

    ARQUIVO TEXT
    Arquivo teste para o desafio da DIO

---
+++ index.js
@@ -0,0 +1,39 @@
+// Função para calcular o nível do herói
+function calcularNivel(nome, experiencia) {
+    let nivel;
+  
+    // Estrutura de decisão para determinar o nível
+    if (experiencia < 1000) {
+      nivel = "Ferro";
+    } else if (experiencia >= 1001 && experiencia <= 2000) {
+      nivel = "Bronze";
+    } else if (experiencia >= 2001 && experiencia <= 5000) {
+      nivel = "Prata";
+    } else if (experiencia >= 5001 && experiencia <= 7000) {
+      nivel = "Ouro";
+    } else if (experiencia >= 7001 && experiencia <= 8000) {
+      nivel = "Platina";
+    } else if (experiencia >= 8001 && experiencia <= 9000) {
+      nivel = "Ascendente";
+    } else if (experiencia >= 9001 && experiencia <= 10000) {
+      nivel = "Imortal";
+    } else {
+      nivel = "Radiante";
+    }
+  
+    // Retorna o nível do herói
+    return nivel;
+  }
+  
+  // Obtém o nome do herói
+  const nomeDoHeroi = prompt("CARLOS");
+  
+  // Obtém a experiência do herói
+  const experienciaDoHeroi = parseInt(prompt("100000"));
+  
+  // Calcula o nível do herói
+  const nivelDoHeroi = calcularNivel(nomeDoHeroi, experienciaDoHeroi);
+  
+  // Exibe a mensagem final
+  console.log(`O Herói de nome **${nomeDoHeroi}** está no nível de **${nivelDoHeroi}**`);
+  
